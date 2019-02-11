ac6155d053eff18f2d4cefeb6a0517d55af5e1ff
CClingain <clc586@nyu.edu>
2019-02-11 16:15:16 -0500

    Fixed dates (for the most part: see comments) and saved out cleaner data

--- Code/2. Data Cleaning.R
+++ Code/2. Data Cleaning.R
@@ -2,14 +2,20 @@
 ###### 2. Data Cleaning #########
 #################################
 
+load("Data/onetrain_stoptimeJan31.Rdata")
+
 # Convert data classes
 onetrain_stoptime$arrival_time <- as.numeric(as.character(onetrain_stoptime$arrival_time))
 onetrain_stoptime$arrival_delay <- as.numeric(as.character(onetrain_stoptime$arrival_delay))
 onetrain_stoptime$departure_time <- as.numeric(as.character(onetrain_stoptime$departure_time))
 onetrain_stoptime$departure_delay <- as.numeric(as.character(onetrain_stoptime$departure_delay))
+# Convert times from # of seconds since POSIX time (1970-01-01 00:00:00)
+class(onetrain_stoptime$arrival_time) = c('POSIXt','POSIXct')
+class(onetrain_stoptime$departure_time) = c('POSIXt', 'POSIXct')
+# This creates some issues for the 0 cells, but will be fixed later
 
 # Subset South Ferry trains only
 onetrain_stoptime_sub <- onetrain_stoptime[onetrain_stoptime$stop_id=="142S",]
 
-# Convert times from # of seconds since POSIX time (1970-01-01 00:00:00)
-class(onetrain_stoptime_sub$arrival_time) = c('POSIXt','POSIXct')
+# Save the current data
+save(onetrain_stoptime_sub, file = "Data/onetrain_stoptimeJan31_clean.Rdata")
