278afe2e9efcee43770e472ac2ff91c34fd03a02
CClingain <clc586@nyu.edu>
2019-02-11 16:20:05 -0500

    Fixed the funky dates for the 0 cells from date conversion

--- Code/2. Data Cleaning.R
+++ Code/2. Data Cleaning.R
@@ -9,10 +9,16 @@ onetrain_stoptime$arrival_time <- as.numeric(as.character(onetrain_stoptime$arri
 onetrain_stoptime$arrival_delay <- as.numeric(as.character(onetrain_stoptime$arrival_delay))
 onetrain_stoptime$departure_time <- as.numeric(as.character(onetrain_stoptime$departure_time))
 onetrain_stoptime$departure_delay <- as.numeric(as.character(onetrain_stoptime$departure_delay))
+
 # Convert times from # of seconds since POSIX time (1970-01-01 00:00:00)
 class(onetrain_stoptime$arrival_time) = c('POSIXt','POSIXct')
 class(onetrain_stoptime$departure_time) = c('POSIXt', 'POSIXct')
-# This creates some issues for the 0 cells, but will be fixed later
+
+# Fix 0 cells to be NA (not sure if this best approach?)
+arrivalsNA <- which(onetrain_stoptime$arrival_time < "2019-01-01")
+onetrain_stoptime$arrival_time[arrivalsNA] <- NA
+departNA <- which(onetrain_stoptime$departure_time < "2019-01-01")
+onetrain_stoptime$departure_time[departNA] <- NA
 
 # Subset South Ferry trains only
 onetrain_stoptime_sub <- onetrain_stoptime[onetrain_stoptime$stop_id=="142S",]
