5c7ac6e51014a12735d6da896dc6fefa3ce51f22
CClingain <clc586@nyu.edu>
2019-02-25 12:35:51 -0500

    fixed code for historical data import

--- Code/1b_Historical_Import.R
+++ Code/1b_Historical_Import.R
@@ -11,3 +11,38 @@ raw <- GET(link)
 
 feed.message <- gtfs_realtime(raw)
 dat <- gtfs_tripUpdates(feed.message) 
+
+onetrain_info <- NULL
+onetrain_stoptime <- NULL
+
+# Extract from lists
+for(i in 1:length(dat)) {
+  temp <- as.data.frame(as.matrix(dat[[i]]$dt_trip_info))
+  onetrain_info <- rbind(onetrain_info, temp)
+  
+  temp2 <- as.data.frame(as.matrix(dat[[i]]$dt_stop_time_update))
+  onetrain_stoptime <- rbind(onetrain_stoptime, temp2)
+}
+
+
+# Convert data classes
+onetrain_stoptime$arrival_time <- as.numeric(as.character(onetrain_stoptime$arrival_time))
+onetrain_stoptime$arrival_delay <- as.numeric(as.character(onetrain_stoptime$arrival_delay))
+onetrain_stoptime$departure_time <- as.numeric(as.character(onetrain_stoptime$departure_time))
+onetrain_stoptime$departure_delay <- as.numeric(as.character(onetrain_stoptime$departure_delay))
+
+# Convert times from # of seconds since POSIX time (1970-01-01 00:00:00)
+class(onetrain_stoptime$arrival_time) <- c('POSIXt','POSIXct')
+class(onetrain_stoptime$departure_time) <- c('POSIXt', 'POSIXct')
+
+# Fix 0 cells to be NA (not sure if this best approach?)
+arrivalsNA <- which(onetrain_stoptime$arrival_time < "2017-12-31")
+onetrain_stoptime$arrival_time[arrivalsNA] <- NA
+departNA <- which(onetrain_stoptime$departure_time < "2017-12-31")
+onetrain_stoptime$departure_time[departNA] <- NA
+
+# Subset South Ferry trains only
+onetrain_stoptime_sub <- onetrain_stoptime[onetrain_stoptime$stop_id=="142S",]
+
+# Save the current data
+# save(onetrain_stoptime_sub, file = paste("Data/Clean/onetrain_stoptime",Sys.Date(),"_clean",".RData", sep = ""))
