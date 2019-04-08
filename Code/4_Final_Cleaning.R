2c7a81ff051069d68ba7761553361f1a4389fdc5
CClingain <clc586@nyu.edu>
2019-04-08 14:38:13 -0400

    initialized final cleaning script

---
+++ Code/4_Final_Cleaning.R
@@ -0,0 +1,7 @@
+########################################
+####### 4. Final Data Cleaning #########
+########################################
+
+# I noticed through the extract/clean process that not all of the predicted
+# arrival times were removed. This script will fix that. 
+
